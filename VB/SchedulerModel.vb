aa288cac0f9505e9fa7e15162ea954d0bbf971ea
DevExpressExampleBot <fakeEmail>
2022-12-17 13:11:39 +0400

    Source auto update [skip ci]

--- VB/SchedulerModel.vb
+++ VB/SchedulerModel.vb
@@ -1,33 +1,50 @@
-﻿Imports System
-
 Namespace SchedulerBindToObservableCollectionWpf
-#Region "#models    "
+
+'#Region "#models    "
     Public Class ModelAppointment
-        Public Property Id() As Object
-        Public Property StartTime() As Date
-        Public Property EndTime() As Date
-        Public Property Subject() As String
-        Public Property Status() As Integer
-        Public Property Description() As String
-        Public Property Label() As Integer
-        Public Property Location() As String
-        Public Property AllDay() As Boolean
-        Public Property EventType() As Integer
-        Public Property RecurrenceInfo() As String
-        Public Property ReminderInfo() As String
-        Public Property ResourceId() As Object
-        Public Property Price() As Decimal
+
+        Public Property Id As Object
+
+        Public Property StartTime As Date
+
+        Public Property EndTime As Date
+
+        Public Property Subject As String
+
+        Public Property Status As Integer
+
+        Public Property Description As String
+
+        Public Property Label As Integer
+
+        Public Property Location As String
+
+        Public Property AllDay As Boolean
+
+        Public Property EventType As Integer
+
+        Public Property RecurrenceInfo As String
+
+        Public Property ReminderInfo As String
+
+        Public Property ResourceId As Object
+
+        Public Property Price As Decimal
+
         Public Sub New()
         End Sub
     End Class
 
     Public Class ModelResource
-        Public Property Id() As Integer
-        Public Property Name() As String
-        Public Property Color() As Integer
+
+        Public Property Id As Integer
+
+        Public Property Name As String
+
+        Public Property Color As Integer
 
         Public Sub New()
         End Sub
     End Class
-#End Region ' #models
+'#End Region  ' #models
 End Namespace
